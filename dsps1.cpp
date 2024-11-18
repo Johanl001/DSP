33a12991a6b6b345c6181529b2cf0e7cc8fadb02
Johanl001 <151047765+Johanl001@users.noreply.github.com>
2024-11-18 16:41:02 +0530

    Add files via upload

---
+++ dsps1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class linearsearch;
+{
+private:
+int A;
+int n[100];
+int target;
+public:
+void accept():
+void search();
+void display();
+};
+
+void accept()
+int A;
+{
+cout<<"Enter the array elements in non decreasing order";
+cin>>A>>endl;
+cout<<"Enter the target element:";
+cin>>target>>endl;
+}
+
+void search() {
+if(int i==target)
+{
+cout<<"Target element successfully found!";
+}
+else
+{
+cout<<"Target element not found!";
+return -1;
+}
+}
+
+void display()
+{
+for(i=0;i<=n;i++);
+{
+cout<<"Element present at index, &i";
+}
+}
+
+int main()
+{
+linearsearch aa;
+aa.accept;
+aa.search;
+aa.display;
+
+return 0;
+}
+
+
+
+
+
