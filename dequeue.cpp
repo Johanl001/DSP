0b235018f0bf7a2bd27cd9a0aa43f2e58fd5b347
Johanl001 <151047765+Johanl001@users.noreply.github.com>
2024-11-18 16:46:09 +0530

    Add files via upload

---
+++ dequeue.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class P
+{
+int q;
+string name, address;
+
+public:
+void acceptorder()
+void serveorder()
+void displayorder()
+void peak()
+}P[100];
+
+void P::acceptorder()
+{
+if(F==-1 && R==-1)
+cout<<"No orders till now!!";
+cout<<"Enter your name, address and quantity:";
+cin>>P[R].name>>P[R].address>>P[R].quantity;
+}
+else if(F==0 && R==0) || (F=R+1)
+{
+cout<<"Your 1st order is placed successfully!!";
+
+void P::serveorder()
+{
+
+
+
